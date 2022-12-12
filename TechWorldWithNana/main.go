1200ed2a3683802eb54884df1374804582034520
SamirPS <akariohsamir@yahoo.com>
2022-12-12 16:58:27 +0100

    scan user input

--- TechWorldWithNana/main.go
+++ TechWorldWithNana/main.go
@@ -5,19 +5,27 @@ import (
 )
 
 func main() {
-	var conferenceName string = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTicket uint = 50
 	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
 	fmt.Printf("Get one of the %d remaining tickets here to attend\n", remainingTickets)
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets uint
 	// ask user for their name
+	fmt.Println("Give your first name")
+	fmt.Scan(&firstName)
+	fmt.Println("Give your last name")
+	fmt.Scan(&lastName)
+	fmt.Println("Give your email")
+	fmt.Scan(&email)
+	fmt.Println("How many tickets")
+	fmt.Scan(&userTickets)
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v booked %d tickets \n", userName, userTickets)
+	fmt.Printf("Thanks you %v %v for booking %v tickets, you will receive a email at %v \n", firstName, lastName, userTickets, email)
 
 }
