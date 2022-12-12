cf0f53bba2698a6b74d3ab9d81e0492aaa2267fd
SamirPS <akariohsamir@yahoo.com>
2022-12-12 20:20:01 +0100

    for loop

--- TechWorldWithNana/main.go
+++ TechWorldWithNana/main.go
@@ -12,26 +12,28 @@ func main() {
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
 	fmt.Printf("Get one of the %d remaining tickets here to attend\n", remainingTickets)
 
-	var bookings []string
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		var bookings []string
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Give your first name")
-	fmt.Scan(&firstName)
-	fmt.Println("Give your last name")
-	fmt.Scan(&lastName)
-	fmt.Println("Give your email")
-	fmt.Scan(&email)
-	fmt.Println("How many tickets")
-	fmt.Scan(&userTickets)
+		fmt.Println("Give your first name")
+		fmt.Scan(&firstName)
+		fmt.Println("Give your last name")
+		fmt.Scan(&lastName)
+		fmt.Println("Give your email")
+		fmt.Scan(&email)
+		fmt.Println("How many tickets")
+		fmt.Scan(&userTickets)
 
-	remainingTickets -= userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets -= userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Thanks you %v %v for booking %v tickets, you will receive a email at %v \n", firstName, lastName, userTickets, email)
-	fmt.Printf("%d tickets remaining for %v\n", remainingTickets, conferenceName)
-	fmt.Println(bookings)
+		fmt.Printf("Thanks you %v %v for booking %v tickets, you will receive a email at %v \n", firstName, lastName, userTickets, email)
+		fmt.Printf("%d tickets remaining for %v\n", remainingTickets, conferenceName)
+		fmt.Printf("Theses are all our bookings: %v\n", bookings)
+	}
 
 }
