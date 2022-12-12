312f1484f3256b52fbff9a2ed16cadcbde287f35
SamirPS <akariohsamir@yahoo.com>
2022-12-12 16:45:19 +0100

    add var

--- TechWorldWithNana/main.go
+++ TechWorldWithNana/main.go
@@ -5,11 +5,19 @@ import (
 )
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTicket = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go Conference"
+	const conferenceTicket int = 50
+	var remainingTickets int = 50
 
 	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
-	fmt.Printf("Get one of the %d tickets here to attend\n", remainingTickets)
+	fmt.Printf("Get one of the %d remaining tickets here to attend\n", remainingTickets)
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %v booked %d tickets \n", userName, userTickets)
 
 }
