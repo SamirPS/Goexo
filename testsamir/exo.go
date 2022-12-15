f6914865da188eada5e0f294302d085531d38ff3
Samir Akarioh <samir.akarioh@canonical.com>
2022-12-15 15:11:16 +0100

    channel fixed

--- testsamir/exo.go
+++ testsamir/exo.go
@@ -5,48 +5,50 @@ import (
 	"sync"
 )
 
-var wg sync.WaitGroup
-
 var Graph = make(map[string][]int)
 
 func main() {
+	var wg sync.WaitGroup
 	Graph["0"] = []int{1, 2}
 	Graph["1"] = []int{2, 3}
 	Graph["2"] = []int{4}
 	Graph["3"] = []int{4}
 	Graph["4"] = []int{2, 5}
 	Graph["5"] = []int{3}
-	wg.Add(1)
-	go BFS(Graph, 1)
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			BFS(Graph, n)
+		}(i)
+
+	}
 	wg.Wait()
 
 }
 
-func contains(s *chan int, element int) bool {
-	for v := range *s {
-		if v == element {
-			return true
-		}
-	}
-	return false
-}
-
 func BFS(Graph map[string][]int, s int) {
 	tovisit := make(chan int, len(Graph))
-	visited := make(chan int, len(Graph))
+	var visited = map[string]bool{}
 
+	fmt.Printf("\nWe begin with the %v node \n", s)
+
+	for i := range Graph {
+		visited[i] = false
+	}
 	tovisit <- s
+	visited[fmt.Sprint(s)] = true
+
 	for len(tovisit) > 0 {
 		node := <-tovisit
-		visited <- node
+		fmt.Printf("%v ", node)
 		for _, v := range Graph[fmt.Sprint(node)] {
-			if !(contains(&tovisit, v) || contains(&visited, v)) {
+			if !visited[fmt.Sprint(v)] {
 				tovisit <- v
+				visited[fmt.Sprint(v)] = true
 			}
+
 		}
 	}
-
-	fmt.Printf("Here the result:%v if we start from %d\n", visited, s)
-	wg.Done()
 
 }
